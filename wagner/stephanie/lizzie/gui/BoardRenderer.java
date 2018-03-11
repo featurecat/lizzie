c32cad261a72c80bbe2d294768820efd9775f6a3
cngoodboy <cngoodboy@qq.com>
2018-03-11 17:36:09 +0800

    Update BoardRenderer.java

--- wagner/stephanie/lizzie/gui/BoardRenderer.java
+++ wagner/stephanie/lizzie/gui/BoardRenderer.java
@@ -127,8 +127,10 @@ public class BoardRenderer {
             }
 
             // mark the last coordinate
-            int[] lastMove = Lizzie.board.getLastMove();
-            if (lastMove != null) {
+            BoardData data = Lizzie.board.getData();
+			
+            if (data.lastMove != null) {
+				int[] lastMove = data.lastMove;
                 int circleRadius = squareSize / 4;
 
                 int stoneX = x + scaledMargin + squareSize * lastMove[0] - circleRadius;
