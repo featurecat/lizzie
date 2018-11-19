8af60104b5c5b94e8ae9fc1d9b62d172d091176c
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2018-11-19 07:55:53 +0100

    Revert "Fix NPE from getLocationOnScreen_NoTreeLock"

--- src/main/java/featurecat/lizzie/gui/LizzieFrame.java
+++ src/main/java/featurecat/lizzie/gui/LizzieFrame.java
@@ -1275,6 +1275,7 @@ public class LizzieFrame extends JFrame {
             new BufferedImage(scrollPane.getWidth(), scrollPane.getHeight(), TYPE_INT_ARGB);
         Graphics2D g2 = cachedCommentImage.createGraphics();
         scrollPane.doLayout();
+        scrollPane.addNotify();
         scrollPane.validate();
         scrollPane.printAll(g2);
         g2.dispose();
