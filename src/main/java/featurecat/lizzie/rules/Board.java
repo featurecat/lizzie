5f3c3e04363e5ac10c5f7e339cbce395eb5a634c
zsalch <zsal@163.com>
2019-03-21 16:35:12 +0800

    Fixed the winrate issue when clear the board

--- src/main/java/featurecat/lizzie/rules/Board.java
+++ src/main/java/featurecat/lizzie/rules/Board.java
@@ -1007,7 +1007,7 @@ public class Board implements LeelazListener {
 
   /** Clears all history and starts over from empty board. */
   public void clear() {
-    Lizzie.leelaz.sendCommand("clear_board");
+    Lizzie.leelaz.clear();
     Lizzie.frame.resetTitle();
     Lizzie.frame.clear();
     initialize();
