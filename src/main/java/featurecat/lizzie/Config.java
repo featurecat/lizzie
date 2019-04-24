df56d7f0f666a93563ae9baf53729d8f10858b76
Administrator <Administrator@DESKTOP-KSJ2DIN>
2019-04-24 12:00:42 +0800

    fix problems

--- src/main/java/featurecat/lizzie/Config.java
+++ src/main/java/featurecat/lizzie/Config.java
@@ -184,7 +184,7 @@ public class Config {
     showCoordinates = uiConfig.optBoolean("show-coordinates");
     replayBranchIntervalSeconds = uiConfig.optDouble("replay-branch-interval-seconds", 1.0);
     colorByWinrateInsteadOfVisits = uiConfig.optBoolean("color-by-winrate-instead-of-visits");
-    boardPositionProportion = uiConfig.optInt("board-postion-proportion", 4);    
+    boardPositionProportion = uiConfig.optInt("board-postion-proportion", 4);
     winrateStrokeWidth = theme.winrateStrokeWidth();
     minimumBlunderBarWidth = theme.minimumBlunderBarWidth();
     shadowSize = theme.shadowSize();
