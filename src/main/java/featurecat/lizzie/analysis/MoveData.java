030b43e425c95f21302dd73f1e2d525c76dc7bf0
zsalch <zsal@163.com>
2019-05-07 18:00:11 +0800

    Add Limit Best Move & Branch Length

--- src/main/java/featurecat/lizzie/analysis/MoveData.java
+++ src/main/java/featurecat/lizzie/analysis/MoveData.java
@@ -32,7 +32,13 @@ public class MoveData {
       if (key.equals("pv")) {
         // Read variation to the end of line
         result.variation = new ArrayList<>(Arrays.asList(data));
-        result.variation = result.variation.subList(i + 1, data.length);
+        result.variation =
+            result.variation.subList(
+                i + 1,
+                (Lizzie.config.limitBranchLength > 0
+                        && data.length - i - 1 > Lizzie.config.limitBranchLength)
+                    ? i + 1 + Lizzie.config.limitBranchLength
+                    : data.length);
         break;
       } else {
         String value = data[++i];
@@ -66,7 +72,7 @@ public class MoveData {
       result.coordinate = match.group(1);
       result.playouts = Integer.parseInt(match.group(2));
       result.winrate = Double.parseDouble(match.group(3));
-      result.variation = Arrays.asList(match.group(4).split(" "));
+      result.variation = Arrays.asList(match.group(4).split(" ", Lizzie.config.limitBranchLength));
       return result;
     }
   }
