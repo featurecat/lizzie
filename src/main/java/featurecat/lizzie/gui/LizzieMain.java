cfa76d31fda948d54a1fc7f92b63d71cce428446
zsalch <zsal@163.com>
2019-07-17 23:01:54 +0800

    Support the macOS dock icon

--- src/main/java/featurecat/lizzie/gui/LizzieMain.java
+++ src/main/java/featurecat/lizzie/gui/LizzieMain.java
@@ -16,6 +16,7 @@ import java.awt.FontMetrics;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.TexturePaint;
@@ -23,6 +24,8 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.Timer;
@@ -190,7 +193,24 @@ public class LizzieMain extends MainFrame {
     WindowPosition.restorePane(Lizzie.config.persistedUi, commentPane);
 
     try {
-      this.setIconImage(ImageIO.read(getClass().getResourceAsStream("/assets/logo.png")));
+      if (System.getProperty("os.name").contains("Mac")
+          && Utils.classExists("com.apple.eawt.Application")) {
+        try {
+          Class c = Class.forName("com.apple.eawt.Application");
+          Method m = c.getDeclaredMethod("getApplication");
+          Object o = m.invoke(null);
+          Method mset = c.getDeclaredMethod("setDockIconImage", Image.class);
+          mset.invoke(o, ImageIO.read(getClass().getResourceAsStream("/assets/logo.png")));
+        } catch (ClassNotFoundException e1) {
+        } catch (NoSuchMethodException e1) {
+        } catch (SecurityException e1) {
+        } catch (IllegalAccessException e1) {
+        } catch (IllegalArgumentException e1) {
+        } catch (InvocationTargetException e1) {
+        }
+      } else {
+        this.setIconImage(ImageIO.read(getClass().getResourceAsStream("/assets/logo.png")));
+      }
     } catch (IOException e) {
       e.printStackTrace();
     }
