featurecat/lizzie

commit 4f460f8254523e416dd82d5e9169feec6a0af2f6

Allow control the suggestion move information (#573)

src/main/java/featurecat/lizzie/Config.java
@@ -66,6 +66,9 @@ public class Config {
   public boolean showSubBoard = true;
   public boolean largeSubBoard = false;
   public boolean startMaximized = true;
+  public boolean showWinrateInSuggestion = true;
+  public boolean showPlayoutsInSuggestion = true;
+  public boolean showScoremeanInSuggestion = true;
 
   public JSONObject config;
   public JSONObject leelazConfig;
@@ -235,6 +238,9 @@ public class Config {
     showKataGoEstimateBySize = uiConfig.optBoolean("show-katago-estimate-bysize", false);
     showKataGoEstimateOnSubbord = uiConfig.optBoolean("show-katago-estimate-onsubbord", true);
     showKataGoEstimateOnMainbord = uiConfig.optBoolean("show-katago-estimate-onmainboard", true);
+    showWinrateInSuggestion = uiConfig.optBoolean("show-winrate-in-suggestion", true);
+    showPlayoutsInSuggestion = uiConfig.optBoolean("show-playouts-in-suggestion", true);
+    showScoremeanInSuggestion = uiConfig.optBoolean("show-scoremean-in-suggestion", true);
 
     if (theme.fontName() != null) fontName = theme.fontName();
 

src/main/java/featurecat/lizzie/gui/BoardRenderer.java
@@ -800,7 +800,7 @@ public class BoardRenderer {
         text = String.format("%.1f", roundedWinrate);
       }
 
-      if (Lizzie.leelaz.isKataGo && Lizzie.config.showKataGoScoreMean) {
+      if (Lizzie.leelaz.isKataGo && Lizzie.config.showScoremeanInSuggestion) {
         double score = textData.move.scoreMean;
         if (Lizzie.board.getHistory().isBlacksTurn()) {
           if (Lizzie.config.showKataGoBoardScoreMean) {
@@ -814,14 +814,74 @@ public class BoardRenderer {
             score = -score;
           }
         }
-        if (Lizzie.config.kataGoNotShowWinrate) {
+        if (!Lizzie.config.showWinrateInSuggestion) {
+          drawString(
+              g,
+              textData.suggestionX,
+              textData.suggestionY
+                  + (Lizzie.config.showPlayoutsInSuggestion
+                      ? (-stoneRadius * 3 / 16)
+                      : stoneRadius / 4),
+              MainFrame.winrateFont,
+              Font.PLAIN,
+              String.format("%.1f", score),
+              stoneRadius,
+              stoneRadius * (Lizzie.config.showPlayoutsInSuggestion ? 1.5 : 1.8),
+              1);
+
+          if (Lizzie.config.showPlayoutsInSuggestion) {
+            drawString(
+                g,
+                textData.suggestionX,
+                textData.suggestionY + stoneRadius * 2 / 5,
+                MainFrame.uiFont,
+                Utils.getPlayoutsString(textData.move.playouts),
+                (float) (stoneRadius * 0.8),
+                stoneRadius * 1.4);
+          }
+        } else {
+          drawString(
+              g,
+              textData.suggestionX,
+              textData.suggestionY
+                  - (Lizzie.config.showPlayoutsInSuggestion ? stoneRadius * 5 / 16 : 0),
+              LizzieFrame.winrateFont,
+              Font.PLAIN,
+              text,
+              stoneRadius,
+              stoneRadius * (Lizzie.config.showPlayoutsInSuggestion ? 1.45 : 1.5),
+              1);
+          if (Lizzie.config.showPlayoutsInSuggestion) {
+            drawString(
+                g,
+                textData.suggestionX,
+                textData.suggestionY + stoneRadius * 2 / 16,
+                MainFrame.uiFont,
+                Utils.getPlayoutsString(textData.move.playouts),
+                (float) (stoneRadius * 0.7),
+                stoneRadius * 1.4);
+          }
+          drawString(
+              g,
+              textData.suggestionX,
+              textData.suggestionY
+                  + (Lizzie.config.showPlayoutsInSuggestion
+                      ? stoneRadius * 11 / 16
+                      : stoneRadius * 2 / 5),
+              LizzieFrame.uiFont,
+              String.format("%.1f", score),
+              (float) (stoneRadius * (Lizzie.config.showPlayoutsInSuggestion ? 0.75 : 0.8)),
+              stoneRadius * (Lizzie.config.showPlayoutsInSuggestion ? 1.3 : 1.4));
+        }
+      } else {
+        if (Lizzie.config.showWinrateInSuggestion && Lizzie.config.showPlayoutsInSuggestion) {
           drawString(
               g,
               textData.suggestionX,
               textData.suggestionY,
               MainFrame.winrateFont,
               Font.PLAIN,
-              String.format("%.1f", score),
+              text,
               stoneRadius,
               stoneRadius * 1.5,
               1);
@@ -835,53 +895,28 @@ public class BoardRenderer {
               (float) (stoneRadius * 0.8),
               stoneRadius * 1.4);
         } else {
-          drawString(
-              g,
-              textData.suggestionX,
-              textData.suggestionY - stoneRadius * 5 / 16,
-              LizzieFrame.winrateFont,
-              Font.PLAIN,
-              text,
-              stoneRadius,
-              stoneRadius * 1.45,
-              1);
-          drawString(
-              g,
-              textData.suggestionX,
-              textData.suggestionY + stoneRadius * 2 / 16,
-              MainFrame.uiFont,
-              Utils.getPlayoutsString(textData.move.playouts),
-              (float) (stoneRadius * 0.7),
-              stoneRadius * 1.4);
-          drawString(
-              g,
-              textData.suggestionX,
-              textData.suggestionY + stoneRadius * 11 / 16,
-              LizzieFrame.uiFont,
-              String.format("%.1f", score),
-              (float) (stoneRadius * 0.75),
-              stoneRadius * 1.3);
+          if (Lizzie.config.showWinrateInSuggestion) {
+            drawString(
+                g,
+                textData.suggestionX,
+                textData.suggestionY + stoneRadius / 4,
+                MainFrame.winrateFont,
+                Font.PLAIN,
+                text,
+                stoneRadius,
+                stoneRadius * (Lizzie.config.showPlayoutsInSuggestion ? 1.5 : 1.6),
+                1);
+          } else if (Lizzie.config.showPlayoutsInSuggestion) {
+            drawString(
+                g,
+                textData.suggestionX,
+                textData.suggestionY + stoneRadius / 6,
+                MainFrame.uiFont,
+                Utils.getPlayoutsString(textData.move.playouts),
+                (float) (stoneRadius * 0.8),
+                stoneRadius * 1.4);
+          }
         }
-      } else {
-        drawString(
-            g,
-            textData.suggestionX,
-            textData.suggestionY,
-            MainFrame.winrateFont,
-            Font.PLAIN,
-            text,
-            stoneRadius,
-            stoneRadius * 1.5,
-            1);
-
-        drawString(
-            g,
-            textData.suggestionX,
-            textData.suggestionY + stoneRadius * 2 / 5,
-            MainFrame.uiFont,
-            Utils.getPlayoutsString(textData.move.playouts),
-            (float) (stoneRadius * 0.8),
-            stoneRadius * 1.4);
       }
     }
   }

src/main/java/featurecat/lizzie/gui/ConfigDialog.java
@@ -177,6 +177,9 @@ public class ConfigDialog extends JDialog {
   public JSlider sldBoardPositionProportion;
   public JTextField txtLimitBestMoveNum;
   public JTextField txtLimitBranchLength;
+  public JCheckBox chkShowWinrateInSuggestion;
+  public JCheckBox chkShowPlayoutsInSuggestion;
+  public JCheckBox chkShowScoremeanInSuggestion;
   public JTextPane tpGtpConsoleStyle;
 
   // Theme Tab
@@ -1120,12 +1123,29 @@ public class ConfigDialog extends JDialog {
     uiTab.add(txtLimitBranchLength);
     txtLimitBranchLength.setColumns(10);
 
+    JLabel lblSuggestionMoveInfo =
+        new JLabel(resourceBundle.getString("LizzieConfig.title.suggestionMoveInfo"));
+    lblSuggestionMoveInfo.setBounds(6, 416, 163, 16);
+    uiTab.add(lblSuggestionMoveInfo);
+    chkShowWinrateInSuggestion =
+        new JCheckBox(resourceBundle.getString("LizzieConfig.title.showWinrateInSuggestion"));
+    chkShowWinrateInSuggestion.setBounds(170, 416, 100, 23);
+    uiTab.add(chkShowWinrateInSuggestion);
+    chkShowPlayoutsInSuggestion =
+        new JCheckBox(resourceBundle.getString("LizzieConfig.title.showPlayoutsInSuggestion"));
+    chkShowPlayoutsInSuggestion.setBounds(270, 416, 100, 23);
+    uiTab.add(chkShowPlayoutsInSuggestion);
+    chkShowScoremeanInSuggestion =
+        new JCheckBox(resourceBundle.getString("LizzieConfig.title.showScoremeanInSuggestion"));
+    chkShowScoremeanInSuggestion.setBounds(370, 416, 100, 23);
+    uiTab.add(chkShowScoremeanInSuggestion);
+
     JLabel lblGtpConsoleStyle =
         new JLabel(resourceBundle.getString("LizzieConfig.title.gtpConsoleStyle"));
-    lblGtpConsoleStyle.setBounds(6, 416, 157, 16);
+    lblGtpConsoleStyle.setBounds(6, 446, 157, 16);
     uiTab.add(lblGtpConsoleStyle);
     tpGtpConsoleStyle = new JTextPane();
-    tpGtpConsoleStyle.setBounds(170, 416, 460, 80);
+    tpGtpConsoleStyle.setBounds(170, 446, 460, 80);
     uiTab.add(tpGtpConsoleStyle);
 
     setBoardSize();
@@ -1147,7 +1167,28 @@ public class ConfigDialog extends JDialog {
     sldBoardPositionProportion.setValue(Lizzie.config.boardPositionProportion);
     txtLimitBestMoveNum.setText(String.valueOf(Lizzie.config.limitBestMoveNum));
     txtLimitBranchLength.setText(String.valueOf(Lizzie.config.limitBranchLength));
+    chkShowWinrateInSuggestion.setSelected(Lizzie.config.showWinrateInSuggestion);
+    chkShowPlayoutsInSuggestion.setSelected(Lizzie.config.showPlayoutsInSuggestion);
+    chkShowScoremeanInSuggestion.setSelected(Lizzie.config.showScoremeanInSuggestion);
     tpGtpConsoleStyle.setText(Lizzie.config.gtpConsoleStyle);
+    chkShowWinrateInSuggestion.addChangeListener(
+        new ChangeListener() {
+          public void stateChanged(ChangeEvent e) {
+            suggestionMoveInfoChanged();
+          }
+        });
+    chkShowPlayoutsInSuggestion.addChangeListener(
+        new ChangeListener() {
+          public void stateChanged(ChangeEvent e) {
+            suggestionMoveInfoChanged();
+          }
+        });
+    chkShowScoremeanInSuggestion.addChangeListener(
+        new ChangeListener() {
+          public void stateChanged(ChangeEvent e) {
+            suggestionMoveInfoChanged();
+          }
+        });
 
     new ComsWorker(this).execute();
     setLocationRelativeTo(getOwner());
@@ -2182,6 +2223,12 @@ public class ConfigDialog extends JDialog {
     }
   }
 
+  private void suggestionMoveInfoChanged() {
+    Lizzie.config.showWinrateInSuggestion = chkShowWinrateInSuggestion.isSelected();
+    Lizzie.config.showPlayoutsInSuggestion = chkShowPlayoutsInSuggestion.isSelected();
+    Lizzie.config.showScoremeanInSuggestion = chkShowScoremeanInSuggestion.isSelected();
+  }
+
   private void setFontValue(JComboBox<String> cmb, String fontName) {
     cmb.setSelectedIndex(0);
     cmb.setSelectedItem(fontName);
@@ -2448,6 +2495,13 @@ public class ConfigDialog extends JDialog {
       Lizzie.config.uiConfig.put("limit-best-move-num", Lizzie.config.limitBestMoveNum);
       Lizzie.config.limitBranchLength = txtFieldIntValue(txtLimitBranchLength);
       Lizzie.config.uiConfig.put("limit-branch-length", Lizzie.config.limitBranchLength);
+      suggestionMoveInfoChanged();
+      Lizzie.config.uiConfig.putOpt(
+          "show-winrate-in-suggestion", Lizzie.config.showWinrateInSuggestion);
+      Lizzie.config.uiConfig.putOpt(
+          "show-playouts-in-suggestion", Lizzie.config.showPlayoutsInSuggestion);
+      Lizzie.config.uiConfig.putOpt(
+          "show-scoremean-in-suggestion", Lizzie.config.showScoremeanInSuggestion);
       Lizzie.config.uiConfig.put("gtp-console-style", tpGtpConsoleStyle.getText());
       Lizzie.config.uiConfig.put("theme", cmbThemes.getSelectedItem());
       writeThemeValues();

src/main/java/featurecat/lizzie/gui/WinrateGraph.java
@@ -158,6 +158,7 @@ public class WinrateGraph {
         int mw = g.getFontMetrics().stringWidth(moveNumString);
         int margin = strokeRadius;
         int mx = x - posx < width / 2 ? x + margin : x - mw - margin;
+        g.setColor(Color.black);
         g.drawString(moveNumString, mx, posy + height - margin);
         g.setStroke(previousStroke);
       }

src/main/resources/l10n/DisplayStrings.properties
@@ -110,6 +110,10 @@ LizzieConfig.title.minimumBlunderBarWidth=Minimum Blunder Bar Width
 LizzieConfig.title.shadowSize=Shadow Size
 LizzieConfig.title.limitBestMoveNum=Limit Best Move Number
 LizzieConfig.title.limitBranchLength=Limit Branch Length
+LizzieConfig.title.suggestionMoveInfo=Suggestion Move Info
+LizzieConfig.title.showWinrateInSuggestion=Winrate
+LizzieConfig.title.showPlayoutsInSuggestion=Playouts
+LizzieConfig.title.showScoremeanInSuggestion=Scoremean
 LizzieConfig.title.gtpConsoleStyle=Gtp Console Style
 LizzieConfig.title.defaultTheme=Default
 LizzieConfig.title.fontName=Font Name

src/main/resources/l10n/DisplayStrings_zh_CN.properties
@@ -93,6 +93,10 @@ LizzieConfig.title.minimumBlunderBarWidth=\u80DC\u7387\u53D8\u5316\u6761\u6700\u
 LizzieConfig.title.shadowSize=\u9634\u5F71\u5927\u5C0F
 LizzieConfig.title.limitBestMoveNum=\u9650\u5236\u9009\u70B9\u4E2A\u6570
 LizzieConfig.title.limitBranchLength=\u9650\u5236\u5206\u652F\u957F\u5EA6
+LizzieConfig.title.suggestionMoveInfo=\u9009\u70B9\u4FE1\u606F
+LizzieConfig.title.showWinrateInSuggestion=\u80DC\u7387
+LizzieConfig.title.showPlayoutsInSuggestion=\u6A21\u62DF\u91CF
+LizzieConfig.title.showScoremeanInSuggestion=\u76EE\u5DEE
 LizzieConfig.title.gtpConsoleStyle=Gtp\u63A7\u5236\u53F0\u98CE\u683C
 LizzieConfig.title.defaultTheme=\u9ED8\u8BA4
 LizzieConfig.title.fontName=\u5B57\u4F53

commit 042ada01ab64b247a4cf17cb4e7e260061be1846

Fix: D key was broken after 4f460f825 (ref. #852)

src/main/java/featurecat/lizzie/Config.java
@@ -50,10 +50,8 @@ public class Config {
   public double minPlayoutRatioForStats = 0.1;
   public boolean showLcbWinrate = false;
 
-  public boolean showKataGoScoreMean = true;
   public boolean showKataGoBoardScoreMean = false;
   public boolean kataGoScoreMeanAlwaysBlack = false;
-  public boolean kataGoNotShowWinrate = false;
   public boolean showKataGoEstimate = false;
   public boolean showKataGoEstimateOnSubboard = true;
   public boolean showKataGoEstimateOnMainboard = true;
@@ -236,10 +234,8 @@ public class Config {
     shadowSize = theme.shadowSize();
     showLcbWinrate = config.getJSONObject("leelaz").optBoolean("show-lcb-winrate");
 
-    showKataGoScoreMean = uiConfig.optBoolean("show-katago-scoremean", true);
     showKataGoBoardScoreMean = uiConfig.optBoolean("show-katago-boardscoremean", false);
     kataGoScoreMeanAlwaysBlack = uiConfig.optBoolean("katago-scoremean-alwaysblack", false);
-    kataGoNotShowWinrate = uiConfig.optBoolean("katago-notshow-winrate", false);
     showKataGoEstimate = uiConfig.optBoolean("show-katago-estimate", false);
     showKataGoEstimateOnSubboard = uiConfig.optBoolean("show-katago-estimate-onsubboard", true);
     showKataGoEstimateOnMainboard = uiConfig.optBoolean("show-katago-estimate-onmainboard", true);
@@ -547,10 +543,8 @@ public class Config {
     ui.put("replay-branch-interval-seconds", 1.0);
     ui.put("gtp-console-style", defaultGtpConsoleStyle);
     ui.put("panel-ui", false);
-    ui.put("show-katago-scoremean", true);
     ui.put("show-katago-boardscoremean", false);
     ui.put("katago-scoremean-alwaysblack", false);
-    ui.put("katago-notshow-winrate", false);
     ui.put("show-katago-estimate", false);
     ui.put("show-katago-estimate-onsubboard", true);
     ui.put("show-katago-estimate-onmainboard", true);

src/main/java/featurecat/lizzie/gui/Input.java
@@ -470,23 +470,19 @@ public class Input implements MouseListener, KeyListener, MouseWheelListener, Mo
         break;
 
       case VK_D:
-        if (Lizzie.leelaz.isKataGo) {
-          if (Lizzie.config.showKataGoScoreMean && Lizzie.config.kataGoNotShowWinrate) {
-            Lizzie.config.showKataGoScoreMean = false;
-            Lizzie.config.kataGoNotShowWinrate = false;
+        if (Lizzie.leelaz.supportScoremean()) {
+          if (Lizzie.config.showScoremeanInSuggestion && !Lizzie.config.showWinrateInSuggestion) {
+            Lizzie.config.showScoremeanInSuggestion = false;
+            Lizzie.config.showWinrateInSuggestion = true;
             break;
           }
-          if (Lizzie.config.showKataGoScoreMean && !Lizzie.config.kataGoNotShowWinrate) {
-            Lizzie.config.kataGoNotShowWinrate = true;
+          if (Lizzie.config.showScoremeanInSuggestion && Lizzie.config.showWinrateInSuggestion) {
+            Lizzie.config.showWinrateInSuggestion = false;
             break;
           }
-          if (Lizzie.config.showKataGoScoreMean) {
-            Lizzie.config.showKataGoScoreMean = false;
-            break;
-          }
-          if (!Lizzie.config.showKataGoScoreMean) {
-            Lizzie.config.showKataGoScoreMean = true;
-            Lizzie.config.kataGoNotShowWinrate = false;
+          if (!Lizzie.config.showScoremeanInSuggestion) {
+            Lizzie.config.showScoremeanInSuggestion = true;
+            Lizzie.config.showWinrateInSuggestion = true;
           }
         } else {
           toggleShowDynamicKomi();