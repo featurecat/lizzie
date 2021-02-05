c2b2ee18357bd55a7d9ea1f2951da912eae37c9b
xiaoyifang <yifang.xiao@outlook.com>
2021-02-05 20:30:34 +0800

    not turn off ponder when finished fast analysis.(invoked by keymap a)

--- src/main/java/featurecat/lizzie/rules/Board.java
+++ src/main/java/featurecat/lizzie/rules/Board.java
@@ -1412,7 +1412,7 @@ public class Board implements LeelazListener {
       if (!history.getNext().isPresent() || isSuccessivePass) {
         // Reached the end...
         toggleAnalysis();
-        Lizzie.leelaz.togglePonder();
+//        Lizzie.leelaz.togglePonder();
       } else if (bestMoves.isEmpty()) {
         // If we get empty list, something strange happened, ignore notification
       } else {
