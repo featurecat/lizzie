25c878b197e08825cbc4abc0f3bbb5710ea8129b
Hiraoka <khi@users.osdn.me>
2021-02-10 20:15:26 +0900

    Fix d0f9d188: right-click menu was disabled in normal UI with KataGo

--- src/main/java/featurecat/lizzie/gui/LizzieFrame.java
+++ src/main/java/featurecat/lizzie/gui/LizzieFrame.java
@@ -1544,7 +1544,7 @@ public class LizzieFrame extends MainFrame {
   }
 
   public boolean openRightClickMenu(int x, int y) {
-    if (Lizzie.leelaz.isKataGo) {
+    if (Lizzie.leelaz.isKataGo && !Lizzie.frame.isMouseOver) {
       return false;
     }
     Optional<int[]> boardCoordinates = boardRenderer.convertScreenToCoordinates(x, y);
