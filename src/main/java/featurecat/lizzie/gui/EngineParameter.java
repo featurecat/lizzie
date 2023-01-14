88d0ae2a5944a4dcba25be46be2561093f25f29d
Hiraoka <khi@users.osdn.me>
2023-01-15 06:10:09 +0900

    Scroll to the top of help message in engine setting

--- src/main/java/featurecat/lizzie/gui/EngineParameter.java
+++ src/main/java/featurecat/lizzie/gui/EngineParameter.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
@@ -15,6 +16,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 
 public class EngineParameter extends JDialog {
@@ -93,6 +95,7 @@ public class EngineParameter extends JDialog {
     txtParams.setFont(font);
     txtParams.setText(configDialog.commandHelp);
     txtParams.setEditable(false);
+    SwingUtilities.invokeLater(() -> txtParams.scrollRectToVisible(new Rectangle(0, 0, 0, 0)));
 
     JLabel lblParameterList =
         new JLabel(configDialog.resourceBundle.getString("LizzieConfig.title.parameterList"));
